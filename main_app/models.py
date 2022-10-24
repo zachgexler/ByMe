64e17d8feb04d3d9bcf358cc6bdb177977dd79eb
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-10-24 07:59:15 -0400

    defining more templates adn views

--- main_app/models.py
+++ main_app/models.py
@@ -6,12 +6,15 @@ INVESTMENTS = (
     ('D', 'Debit'),
 )
 
+
 class Investment(models.Model):
     name = models.CharField(max_length=50)
     amount = models.FloatField(max_length=50)
 
+
 def get_absolute_url(self):
     return reverse('investments_detail', kwargs={'pk': self.id})
+
 
 class Symbol(models.Model):
   name = models.CharField(max_length=100)
@@ -20,20 +23,22 @@ class Symbol(models.Model):
   price = models.FloatField()
   investment = models.ManyToManyField(Investment)
 
+
 def __str__(self):
     return self.name
 
+
 def get_absolute_url(self):
     return reverse('detail', kwargs={'symbol_id': self.id})
+
 
 class Investing(models.Model):
     date = models.DateField("Investing Date")
     amount = models.IntegerField(
         max_length=1000000,
-        default=INVESTMENTS[0][0]
-    )
-
+        default=INVESTMENTS[0][0])
     symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+
 
 def __str__(self):
     return f"{self.get_investing_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -6,56 +6,87 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from main_app.models import Symbol, Investment, Investing
 from .forms import InvestingForm
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 def about(request):
   return render(request, 'about.html')
 
+
 def symbols_index(request):
   symbols = Symbol.objects.all()
-  return render(request, 'symbols/index.html', { 'symbols': symbols })
+  return render(request, 'symbols/index.html', {'symbols': symbols})
+
 
 def symbols_detail(request, symbol_id):
   symbols = Symbol.objects.get(id=symbol_id)
   investing_form = InvestingForm()
-  investments_symbol_doesnt_have = Investment.objects.exclude(id__in = symbols.investments.all().values_list('id'))
+  investments_symbol_doesnt_have = Investment.objects.exclude(
+      id__in=symbols.investments.all().values_list('id'))
+  return render(request, 'symbols/detail.html', {
+      'symbol': symbols,
+      'investing_form': investing_form,
+      'investments': investments_symbol_doesnt_have,
+  })
+
+
+def add_investing(request, symbol_id):
+  form = InvestingForm(request.POST)
+  if form.is_valid():
+    new_investing = form.save(commit=False)
+    new_investing.symbol_id = symbol_id
+    new_investing.save()
+  return redirect('detail', symbol_id=symbol_id)
+
 
 def assoc_investment(request, symbol_id, investment_id):
    Investment.objects.get(id=symbol_id).investments.add(investment_id)
    return redirect('detail', symbol_id=symbol_id)
+
 
 class SymbolCreate(CreateView):
     model = Symbol
     fields = ['name', 'breed', 'description', 'age']
     success_url = '/symbols'
 
+
 class SymbolUpdate(UpdateView):
   model = Symbol
   fields = ['name', 'description', 'price', 'investment']
+
 
 class SymbolDelete(DeleteView):
   model = Symbol
   success_url = '/symbols'
 
+
 class SymbolCreate(CreateView):
     model = Symbol
-    fields = ('name','description', 'price', 'investment')
+    fields = ('name', 'description', 'price', 'investment')
+
+
+class InvestmentCreate(CreateView):
+    model = Investment
+    fields = ('name', 'amount')
+
 
 class InvestmentUpdate(UpdateView):
     model = Investment
     fields = ('symbol', 'price')
 
+
 class InvestmentDelete(DeleteView):
     model = Investment
     success_url = '/investments/'
+
 
 class InvestmentDetail(DetailView):
     model = Investment
     template_name = 'investments/detail.html'
 
+
 class InvestmentList(ListView):
     model = Investment
     template_name = 'investments/index.html'
-
-  
