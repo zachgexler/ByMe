0faee125c9a3aa493697b13383f171be551850f4
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-10-24 06:32:10 -0400

    updating views/models adding templates

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.urls import reverse
-# Create your models here.
+
+INVESTMENTS = (
+    ('C', 'Credit'),
+    ('D', 'Debit'),
+)
 
 class Investment(models.Model):
     name = models.CharField(max_length=50)
@@ -18,3 +22,22 @@ class Symbol(models.Model):
 
 def __str__(self):
     return self.name
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'symbol_id': self.id})
+
+class Investing(models.Model):
+    date = models.DateField("Investing Date")
+    amount = models.IntegerField(
+        max_length=1000000,
+        default=INVESTMENTS[0][0]
+    )
+
+    symbol = models.ForeignKey(Symbol, on_delete=models.CASCADE)
+
+def __str__(self):
+    return f"{self.get_investing_display()} on {self.date}"
+
+
+class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from main_app.models import Symbol, Investment
+from main_app.models import Symbol, Investment, Investing
 from .forms import InvestingForm
 
 def home(request):
@@ -11,5 +11,47 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def symbols_detail(request, symbol_id):
+  symbols = Symbol.objects.get(id=symbol_id)
+  investing_form = InvestingForm()
+  investments_symbol_doesnt_have = Investment.objects.exclude(id__in = symbols.investments.all().values_list('id'))
+
+def assoc_investment(request, symbol_id, investment_id):
+   Investment.objects.get(id=symbol_id).investments.add(investment_id)
+   return redirect('detail', symbol_id=symbol_id)
+
+class SymbolCreate(CreateView):
+    model = Symbol
+    fields = ['name', 'breed', 'description', 'age']
+    success_url = '/symbols'
+
+class SymbolUpdate(UpdateView):
+  model = Symbol
+  fields = ['name', 'description', 'price', 'investment']
+
+class SymbolDelete(DeleteView):
+  model = Symbol
+  success_url = '/symbols'
+
+class SymbolCreate(CreateView):
+    model = Symbol
+    fields = ('name','description', 'price', 'investment')
+
+class InvestmentUpdate(UpdateView):
+    model = Investment
+    fields = ('symbol', 'price')
+
+class InvestmentDelete(DeleteView):
+    model = Investment
+    success_url = '/investments/'
+
+class InvestmentDetail(DetailView):
+    model = Investment
+    template_name = 'investments/detail.html'
+
+class InvestmentList(ListView):
+    model = Investment
+    template_name = 'investments/index.html'
 
   
