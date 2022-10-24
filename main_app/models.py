263c6ea371ae0241d6a425428d7b93b658b8c8c4
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-10-24 05:44:47 -0400

    added urls/view/models including urls I know I'll need (CRUD)

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Symbol(models.Model):
+  name = models.CharField(max_length=100)
+  symbol = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  price = models.FloatField()
+  investment = models.ManyToManyField(Investment)
+
+def __str__(self):
+    return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('symbols/', views.symbols_index, name='index'),
+    path('symbols/<int:symbols_id>/', views.symbols_detail, name='detail'),
+    path('symbols/create/', views.SymbolCreate.as_view(), name='symbols_create'),
+    path('symbols/<int:pk>/update', views.SymbolUpdate.as_view(), name='symbols_update'),
+    path('symbols/<int:pk>/delete', views.SymbolDelete.as_view(), name='symbols_delete'),
+    path('symbols/<int:symbols_id>/add_invetment/', views.add_investment, name='add_investment'),
 ]
